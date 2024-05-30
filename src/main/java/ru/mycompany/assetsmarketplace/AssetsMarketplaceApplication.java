0424eb723f97ca5168587ea4deff613002aeb0b1
ArseniyBeglov <HUDAIDOTT@yandex.ru>
2024-05-31 00:21:27 +0300

    initial commit

---
+++ src/main/java/ru/mycompany/assetsmarketplace/AssetsMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package ru.mycompany.assetsmarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetsMarketplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssetsMarketplaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/mycompany/assetsmarketplace/AssetsMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mycompany.assetsmarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetsMarketplaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
